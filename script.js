644d721ee2ac8ff602c18b20404baa4740555c35
Alexander <ogganessyan.808@gmail.com>
2022-11-18 12:31:45 +0400

    Bug fixed 1

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ let sliderContainer = document.querySelector('.homeContent-slider')
 let sliderItems = document.querySelectorAll('.homeContent-sliderItem')
 let radioCheckboxes = document.querySelectorAll('.homeContent-checkboxes > input')
 let about = document.querySelector('.about')
+let aboutItems = document.querySelectorAll('.aboutItems')
 let action = document.querySelector('.action')
 let actionItems = document.querySelectorAll('.action-item')
 let resultLeftContainer = document.querySelector('.blueLoc')
@@ -20,7 +21,7 @@ let resultCount = document.querySelectorAll('.resultCount')
 let services = document.querySelector('.services')
 let serviceUpperItems = document.querySelectorAll('.upperContainer')
 let serviceLowerItems = document.querySelectorAll('.lowerContainer')
-let servicesOpen = document.querySelectorAll('.servicesOpen')
+let servicesItems = document.querySelectorAll('.servicesItems')
 let videoContainer = document.querySelector('.video')
 let teamItems = document.querySelectorAll('.teamItem')
 
@@ -59,27 +60,23 @@ function changeLink(parent) {
     }
 }
 
-
 function radioChecked(c) {
     radioCheckboxes.forEach((item, index) => {
         c == index ? item.checked = true : item.checked = false
     })
 }
-
 function sliderContentAnim(c) {
     sliderItems.forEach((item, index) => {
         if (index === c) {
             item.children[1].children[0].classList.add('rightTextAnim')
             item.children[1].children[1].classList.add('leftTextAnim')
             item.children[1].children[2].classList.add('buttonAnim')
-
             setTimeout(() => {
                 item.children[1].children[0].classList.remove('rightTextAnim')
                 item.children[1].children[1].classList.remove('leftTextAnim')
                 item.children[1].children[2].classList.remove('buttonAnim')
             }, 1000)
         }
-
     })
 }
 
@@ -162,29 +159,14 @@ videoContainer.children[0].onclick = () => {
     videoContent.style.display = 'flex'
     videoContent.children[0].play()
     document.body.style.overflowY = 'hidden'
-    videoBlock.onclick = () => {
-        if (videoContent.children[0].paused) {
-            videoContent.children[0].pause()
-        }
-        else {
-            videoContent.children[0].play()
-        }
+    videoBlock.onclick = () => videoBlock.paused ?  videoBlock.pause() : videoBlock.play()
     }
-    videoShut.onclick = () => {
+    effVid.onclick = () =>{
         videoContent.style.display = 'none'
         document.body.style.overflowY = 'unset'
-        videoContent.children[0].pause()
-        videoContent.children[0].currentTime = 0
+        videoBlock.pause()
+        videoBlock.currentTime = 0
     }
-}
-
-onTop.onclick = () => {
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: 'smooth'
-    });
-}
 
 burgerParent.onclick = () => {
     burgerItems.forEach(it => {
@@ -193,7 +175,7 @@ burgerParent.onclick = () => {
     })
 }
 
-servicesOpen.forEach((item, index, arr) => {
+servicesItems.forEach((item, index, arr) => {
     servicesGallery.children[0].appendChild(serviceGalleryText)
     item.onclick = () => {
         servicesGallery.style.display = 'flex'
@@ -203,41 +185,33 @@ servicesOpen.forEach((item, index, arr) => {
         galleryItemStyle.background = `url(img/service-${imgCount}.jpg)`
         serviceGalleryText.innerHTML = `Image ${index + 1} of ${arr.length}`
         galleryLeft.onclick = () => {
-            galleryItemStyle.background = `url(img/service-${imgCount -= 1}.jpg)`
-            if (imgCount < 0) {
-                galleryItemStyle.background = `url(img/service-${imgCount = 5}.jpg)`
-            }
+            imgCount <= 0 ?  galleryItemStyle.background = `url(img/service-${imgCount = 5}.jpg)` :  galleryItemStyle.background = `url(img/service-${imgCount -= 1}.jpg)`
             serviceGalleryText.innerHTML = `Image ${imgCount + 1} of ${arr.length}`
         }
         galleryRight.onclick = () => {
-            galleryItemStyle.background = `url(img/service-${imgCount += 1}.jpg)`
-            if (imgCount > 5) {
-                galleryItemStyle.background = `url(img/service-${imgCount = 0}.jpg)`
-            }
+            imgCount >= 5 ? galleryItemStyle.background = `url(img/service-${imgCount = 0}.jpg)`: galleryItemStyle.background = `url(img/service-${imgCount += 1}.jpg)`
             serviceGalleryText.innerHTML = `Image ${imgCount + 1} of ${arr.length}`
         }
     }
-    galleryShut.onclick = () => {
+    effGal.onclick = () =>{
         servicesGallery.style.display = 'none'
         document.body.style.overflowY = 'visible'
     }
 })
 
 window.addEventListener('scroll', () => {
-
     const y = window.scrollY
     y > preHeader.clientHeight ? header.setAttribute('style', 'width:100%; box-shadow: 0 5px 5px rgba(0,0,0,0.3);') : header.setAttribute('style', `width: ${document.body.clientWidth < 991 ? '100%' : '90%'}; box-shadow: none;`)
+    y > about.offsetTop - about.clientHeight - header.clientHeight ? onTop.classList.add('hideShow') : onTop.classList.remove('hideShow') 
     addScrollAnim(homeContent, -1, 'home')
-
-    addScrollAnim(about.children[0], about.offsetTop - about.clientHeight, 'animation1')
-    addScrollAnim(about.children[1], about.offsetTop - about.clientHeight, 'animation1')
-    addScrollAnim(onTop, about.offsetTop - about.clientHeight - header.clientHeight, 'hideShow')
+    aboutItems.forEach(item => addScrollAnim(item, about.offsetTop - about.clientHeight, 'animation1' ))
     serviceUpperItems.forEach(item => addScrollAnim(item, services.offsetTop - item.clientHeight, 'animation1'))
     serviceLowerItems.forEach(item => addScrollAnim(item, services.offsetTop + header.clientHeight, 'animation1'))
     teamItems.forEach(item => addScrollAnim(item, item.parentElement.offsetTop - item.parentElement.clientHeight - (header.clientHeight * 2), 'animation1'))
     actionItems.forEach(item => addScrollAnim(item, about.offsetTop - about.clientHeight - action.clientHeight, 'animation1'))
     addScrollAnim(videoContainer, videoContainer.offsetTop - videoContainer.clientHeight - (header.clientHeight * 2), 'hideShow')
     addScrollAnim(footer, footer.offsetTop - footer.clientHeight - (header.clientHeight * 2), 'hideShow')
+
     if (y > about.offsetTop - header.clientHeight) {
         intCount++
         resultLeftContainer.classList.add('resultLeftContainer')
